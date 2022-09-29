f5a699307009b47c687bfead3ca17112eb1a401c
David Vetter <ddvetter23@gmail.com>
2022-09-29 14:05:36 -0500

    added skeleton, store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Checkout from '../Checkout/Checkout';
+import SelectPizza from '../SelectPizza/SelectPizza';
+import CustomerForm from '../CustomerForm/CustomerForm';
+import AdminOrder from '../AdminOrders/AdminOrder';
 
 function App() {
+
+  const dispatch = useDispatch();
+function test() {
+  dispatch({
+    type: 'SET_CUSTOMER_INFO',
+    payload: {
+      customer_name: 'John',
+      street_address: '123 St W',
+      city: 'ANother CIty',
+      type: 'Delivery',
+
+    }
+  })
+}
+useEffect(() => {
+  test();
+}, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      <SelectPizza />
+      <CustomerForm />
+      <Checkout />
+      <AdminOrders />
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
--- src/index.js
+++ src/index.js
@@ -2,5 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultOrder = {
+        customer_name: "Donatello",
+        street_address: "20 W 34th St",
+        city: "New York",
+        zip: "10001",
+        total: "27.98",
+        type: "Pickup",
+        pizzas: [{
+          id: "1",
+          quantity: "1"
+        },{
+          id: "2",
+          quantity: "1"
+        }]
+};
+
+const menuReducer = (state = [], action) => {
+    if (action.type === 'SET_MENU') {
+        return action.payload;
+    }
+    return state;
+}
+
+const orderReducer = (state = defaultOrder, action) => {
+    if (action.type === 'SET_PIZZAS_TOTAL') {
+        let pizzas = action.payload[0];
+        let orderTotal = action.payload[1];
+        let updatedOrder = {...state};
+        updatedOrder.total = orderTotal;
+        updatedOrder.pizzas = pizzas;
+        console.log('This is updatedOrder in SET_PIZZA: ', updatedOrder);
+        return updatedOrder;
+    } else if (action.type === 'SET_CUSTOMER_INFO') {
+        let customer_name = action.payload.customer_name;
+        let street_address = action.payload.street_address;
+        let city = action.payload.city;
+        let zip = action.payload.zip;
+        let type = action.payload.type;
+        let updatedCustomer = {...state};
+        updatedCustomer = {
+            customer_name,
+            street_address,
+            city,
+            zip,
+            total: state.total,
+            type,
+            pizzas: state.pizzas
+        }
+        console.log('This is updatedCustomer in SET Customer: ', updatedCustomer);
+        return updatedCustomer;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        menu: menuReducer,
+        order: orderReducer
+    }),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
